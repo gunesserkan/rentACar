720075f10f92bd37775f6e2110fd2a49e54a481b
Serkan Güneş <49815142+gunesserkan@users.noreply.github.com>
2023-03-05 18:31:50 +0300

    d

--- rentacar/src/main/java/kodlama/io/rentacar/dataAccess/abstracts/BrandRepository.java
+++
@@ -1,9 +0,0 @@
-package kodlama.io.rentacar.dataAccess.abstracts;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import kodlama.io.rentacar.entities.concretes.Brand;
-
-public interface BrandRepository extends JpaRepository <Brand,Integer>{
-        
-}

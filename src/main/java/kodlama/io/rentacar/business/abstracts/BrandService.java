07209d4c7ff4e4bb6d17cf133b38cc4f283447b2
Serkan Güneş <49815142+gunesserkan@users.noreply.github.com>
2023-03-05 22:57:28 +0300

    Ekleme ve Güncellemeler yapıldı

--- src/main/java/kodlama/io/rentacar/business/abstracts/BrandService.java
+++ src/main/java/kodlama/io/rentacar/business/abstracts/BrandService.java
@@ -3,9 +3,14 @@ package kodlama.io.rentacar.business.abstracts;
 import java.util.List;
 
 import kodlama.io.rentacar.business.requests.CreateBrandRequest;
+import kodlama.io.rentacar.business.requests.UpdateBrandRequest;
 import kodlama.io.rentacar.business.responses.GetAllBrandsResponse;
+import kodlama.io.rentacar.business.responses.GetByIdBrandResponse;
 
 public interface BrandService {
     List<GetAllBrandsResponse> getAll();
+    GetByIdBrandResponse getById(int id);
     void addd(CreateBrandRequest createBrandRequest);
+    void update(UpdateBrandRequest updateBrandRequest);
+    void delete(int id);
 }
--- src/main/java/kodlama/io/rentacar/business/concretes/BrandManager.java
+++ src/main/java/kodlama/io/rentacar/business/concretes/BrandManager.java
@@ -1,46 +1,60 @@
 package kodlama.io.rentacar.business.concretes;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import kodlama.io.rentacar.business.abstracts.BrandService;
 import kodlama.io.rentacar.business.requests.CreateBrandRequest;
+import kodlama.io.rentacar.business.requests.UpdateBrandRequest;
 import kodlama.io.rentacar.business.responses.GetAllBrandsResponse;
+import kodlama.io.rentacar.business.responses.GetByIdBrandResponse;
+import kodlama.io.rentacar.core.utilities.mappers.ModelMapperService;
 import kodlama.io.rentacar.dataAccess.abstracts.BrandRepository;
 import kodlama.io.rentacar.entities.concretes.Brand;
+import lombok.AllArgsConstructor;
 
 @Service
+@AllArgsConstructor
 public class BrandManager implements BrandService {
-    private BrandRepository brandRepository;
+	private BrandRepository brandRepository;
+	private ModelMapperService modelMapperService;
 
-    @Autowired
-    public BrandManager(BrandRepository brandRepository) {
-        this.brandRepository = brandRepository;
-    }
+	@Override
+	public List<GetAllBrandsResponse> getAll() {
+		List<Brand> brands = brandRepository.findAll();
+		List<GetAllBrandsResponse> brandsResponse = brands.stream()
+				.map(brand -> this.modelMapperService.forResponse().map(brand, GetAllBrandsResponse.class))
+				.collect(Collectors.toList());
 
-    @Override
-    public List<GetAllBrandsResponse> getAll() {
-        List<Brand> brands = brandRepository.findAll();
-        List<GetAllBrandsResponse> brandsResponse = new ArrayList<GetAllBrandsResponse>();
+		return brandsResponse;
+	}
 
-        for (Brand brand : brands) {
-            GetAllBrandsResponse responseItem = new GetAllBrandsResponse();
-            responseItem.setId(brand.getId());
-            responseItem.setName(brand.getName());
+	@Override
+	public void addd(CreateBrandRequest createBrandRequest) {
+		Brand brand = this.modelMapperService.forRequest().map(createBrandRequest, Brand.class);
+		this.brandRepository.save(brand);
+	}
 
-            brandsResponse.add(responseItem);
-        }
-        return brandsResponse;
-    }
+	@Override
+	public GetByIdBrandResponse getById(int id) {
+		Brand brand = this.brandRepository.findById(id).orElseThrow();
+		GetByIdBrandResponse response = this.modelMapperService.forResponse().map(brand, GetByIdBrandResponse.class);
+		return response;
+	}
 
-    @Override
-    public void addd(CreateBrandRequest createBrandRequest) {
-        Brand brand=new Brand();
-        brand.setName(createBrandRequest.getName());
-        this.brandRepository.save(brand);
-    }
+	@Override
+	public void update(UpdateBrandRequest updateBrandRequest) {
+		Brand brand = this.modelMapperService.forRequest().map(updateBrandRequest, Brand.class);
+		this.brandRepository.save(brand);
+
+	}
+
+	@Override
+	public void delete(int id) {
+		this.brandRepository.deleteById(id);
+
+	}
 
 }
